b1983fbaf64bcc3ae4e8080d0b3fb0be9154b82b
timoteus404 <yakhyoev.temur@mail.ru>
2021-03-07 01:07:26 +0500

    Initial commit

---
+++ src/main/java/com/example/udemy_class/UdemyClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/udemy_class/UdemyClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemy_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
